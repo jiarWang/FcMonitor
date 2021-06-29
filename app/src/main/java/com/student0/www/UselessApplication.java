3c4225ce801bdaf4fa5411cd7efa24e53c9d4cc9
飞彻 <wj317968@alibaba-inc.com>
2021-06-29 22:38:33 +0800

    [feature][1.0.0完成]

---
+++ app/src/main/java/com/student0/www/UselessApplication.java
@@ -0,0 +1,15 @@
+package com.student0.www;
+
+import android.app.Application;
+
+/**
+ * @createDate: 6/29/21
+ * @author: 飞彻
+ * @description:
+ */
+public class UselessApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
